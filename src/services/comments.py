0fe54d1225eb230fcaf188f89a35d736f67ed260
Leonid Gurianov <leonid.11521@gmail.com>
2026-05-19 21:09:56 +0300

    delete comments in services/comments

--- src/services/comments.py
+++ src/services/comments.py
@@ -1,19 +1,20 @@
 from uuid import UUID
 from http import HTTPStatus
 
+from flask import current_app
+
 from src.core.db import db
 from src.core.exceptions import ServiceError
 from src.models.comment import Comment
 
-
-# from src.services.resource_client import ResourceClient
+from src.services.resource_client import ResourceClient
 
 
 class CommentService:
     def create_comment(self, text: str, user_id: UUID, movie_id: UUID) -> Comment:
-        # resource_client = ResourceClient(current_app.config["RESOURCE_SERVICE_URL"])
-        # if not resource_client.movie_exists(movie_id):
-        #     raise ServiceError("Movie not found in resource-service", status_code=404)
+        resource_client = ResourceClient(current_app.config["RESOURCE_SERVICE_URL"])
+        if not resource_client.movie_exists(movie_id):
+             raise ServiceError("Movie not found in resource-service", status_code=404)
 
         comment = Comment(text=text, user_id=user_id, movie_id=movie_id)
         db.session.add(comment)
